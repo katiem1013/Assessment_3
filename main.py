4c736e333ec533ea4c5d821d9127c5e961fdb407
katiem1013 <katiem1013>
2023-03-16 18:56:52 +0000

    Players can now shoot bullets, need to change the directions they shoot in

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ pygame.display.set_caption('Assessment 3')
 clock = pygame.time.Clock()  # clock to set the frame rate
 
 # screen flipping variables
-colour = 1  # 1 is Black, 2 is White
+colour = 1  # 1 has player 1s platform at the bottom, 2 has the platform at the top
 
 # player variables
 player_1_velocity = 5
 player_2_velocity = -5
 speed = 8
+bullet_speed_1 = -5
+bullet_speed_2 = 5
 
 # background colours
 background_black = pygame.image.load('Graphics/BackgroundBlack.png').convert()
@@ -30,7 +32,6 @@ floor_black_location_x = 0
 floor_white_location_y = 0
 floor_white_location_x = 960
 
-
 # background rects
 background_white_rect = background_white.get_rect()
 background_black_rect = background_black.get_rect()
@@ -41,8 +42,7 @@ active_colour = 229, 255, 204
 clicking = False
 
 
-
-
+# fun little drawing feature, doesn't do anything gameplay wise
 def draw_painting(paints):
     for i in range(len(paints)):
         pygame.draw.circle(screen, paints[i][0], paints[i][1], paints[i][2])
@@ -62,15 +62,29 @@ class Player1(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.health = 100
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         self.rect.y += self.y_velocity
+        cooldown = 300  # milliseconds
+
         # movement inputs
         key = pygame.key.get_pressed()
         if key[pygame.K_a] and self.rect.left > 0:
             self.rect.x -= speed
         if key[pygame.K_d] and self.rect.right < screen_width/2:
             self.rect.x += speed
+
+        time_now = pygame.time.get_ticks()
+
+        if colour == 2 and key[pygame.K_w] and time_now - self.last_shot > cooldown:
+            bullet = Bullets(self.rect.centerx, self.rect.bottom)
+            bullet_group.add(bullet)
+            self.last_shot = time_now
+        if colour == 1 and key[pygame.K_w] and time_now - self.last_shot > cooldown:
+            bullet = Bullets(self.rect.centerx, self.rect.top)
+            bullet_group.add(bullet)
+            self.last_shot = time_now
 
 
 class Player2(pygame.sprite.Sprite):
@@ -84,15 +98,43 @@ class Player2(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.health = 100
+        self.last_shot = pygame.time.get_ticks()
 
     def update(self):
         self.rect.y += self.y_velocity
+        cooldown = 300  # milliseconds
+
         # movement inputs
         key = pygame.key.get_pressed()
         if key[pygame.K_LEFT] and self.rect.left > screen_width/2:
             self.rect.x -= speed
         if key[pygame.K_RIGHT] and self.rect.right < screen_width:
             self.rect.x += speed
+
+        time_now = pygame.time.get_ticks()
+
+        if colour == 1 and key[pygame.K_UP] and time_now - self.last_shot > cooldown:
+            bullet = Bullets(self.rect.centerx, self.rect.top)
+            bullet_group.add(bullet)
+            self.last_shot = time_now
+        if colour == 2 and key[pygame.K_UP] and time_now - self.last_shot > cooldown:
+            bullet = Bullets(self.rect.centerx, self.rect.bottom)
+            bullet_group.add(bullet)
+            self.last_shot = time_now
+
+
+class Bullets(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('Graphics/Bullet.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+    def update(self):
+        self.rect.y += bullet_speed_1
+        self.rect.y += bullet_speed_2
+        if self.rect.bottom < 0:
+            self.kill()
 
 
 def colour_change():
@@ -101,21 +143,27 @@ def colour_change():
     global clicking
     global floor_white_location_y
     global floor_black_location_y
+    global bullet_speed_1
+    global bullet_speed_2
 
     key = pygame.key.get_pressed()
-    if key[pygame.K_1] and colour == 2:
+    if key[pygame.K_COMMA] and colour == 2:
         colour = 1
         player.y_velocity = 5
         player2.y_velocity = -5
         painting.clear()
+        bullet_speed_1 = -5
+        bullet_speed_2 = 5
         floor_white_location_y = 0
         floor_black_location_y = 905
 
-    if key[pygame.K_2] and colour == 1:
+    if key[pygame.K_PERIOD] and colour == 1:
         colour = 2
         player.y_velocity = -5
         player2.y_velocity = 5
         painting.clear()
+        bullet_speed_1 = 5
+        bullet_speed_2 = -5
         floor_white_location_y = 905
         floor_black_location_y = 0
 
@@ -134,6 +182,7 @@ player = Player1(int(screen_width / 4), screen_height - 500)
 player2 = Player2(int(screen_width / 1.3), screen_height - 500)
 player_group.add(player, player2)
 
+bullet_group = pygame.sprite.Group()
 
 run = True
 while run:
@@ -152,6 +201,9 @@ while run:
     # allows the players to run within the game
     player_group.update()
     player_group.draw(screen)
+
+    bullet_group.update()
+    bullet_group.draw(screen)
 
     # allows key to be used instead of typing out the whole thing
     key = pygame.key.get_pressed()
