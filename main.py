0ed710d20b6a6d5b6d5cfc4e1e44750cfe8ec791
katiem1013 <katiem1013>
2023-03-31 15:04:56 +0100

    Decided to change how the game will play, took out two player (unsure if its just for now or forever) and am about to add in a platforming level.

--- main.py
+++ main.py
@@ -14,51 +14,30 @@ clock = pygame.time.Clock()  # clock to set the frame rate
 colour = 1  # 1 has player 1s platform at the bottom, 2 has the platform at the top
 
 # player variables
-player_1_velocity = 5
-player_2_velocity = -5
+player_velocity = 5
 speed = 8
-bullet_speed_1 = -5
-bullet_speed_2 = 5
+bullet_speed = -5
 
 # background colours
 background_black = pygame.image.load('Graphics/BackgroundBlack.png').convert()
 background_white = pygame.image.load('Graphics/BackgroundWhite.png').convert()
 
-# floor locations for the black flooring
-floor_black_location_y = 905
-floor_black_location_x = 0
-
-# floor locations for the white flooring
-floor_white_location_y = 0
-floor_white_location_x = 960
-
 # background rects
 background_white_rect = background_white.get_rect()
 background_black_rect = background_black.get_rect()
 
-# variables for drawing on the screen
-painting = []
-active_colour = 229, 255, 204
-clicking = False
 
-
-# fun little drawing feature, doesn't do anything gameplay wise
-def draw_painting(paints):
-    for i in range(len(paints)):
-        pygame.draw.circle(screen, paints[i][0], paints[i][1], paints[i][2])
-
-
-class Player1(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     global colour
-    global player_1_velocity
+    global player_velocity
 
     def __init__(self, x, y):
         super().__init__()
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Graphics/Player.png')
+        self.image = pygame.image.load('Graphics/Player.png').convert()
         self.x = 0
         self.y = 0
-        self.y_velocity = player_1_velocity
+        self.y_velocity = player_velocity
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
         self.health = 100
@@ -67,10 +46,9 @@ class Player1(pygame.sprite.Sprite):
     def update(self):
         self.rect.y += self.y_velocity
         cooldown = 500  # milliseconds
-        global bullet_speed_1
-
-        # movement inputs
+        global bullet_speed
         key = pygame.key.get_pressed()
+        # movement inputs
         if key[pygame.K_a] and self.rect.left > 0:
             self.rect.x -= speed
         if key[pygame.K_d] and self.rect.right < screen_width/2:
@@ -81,129 +59,51 @@ class Player1(pygame.sprite.Sprite):
         if colour == 1 and key[pygame.K_w] and time_now - self.last_shot > cooldown:
             bullet = Bullets(self.rect.centerx, self.rect.top)
             bullet_group.add(bullet)
-            bullet_speed_1 = -5
+            bullet_speed = -5
             self.last_shot = time_now
 
         if colour == 2 and key[pygame.K_w] and time_now - self.last_shot > cooldown:
             bullet = Bullets(self.rect.centerx, self.rect.bottom)
             bullet_group.add(bullet)
-            bullet_speed_1 = 5
-            self.last_shot = time_now
-
-
-class Player2(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        super().__init__()
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Graphics/Player.png')
-        self.x = 0
-        self.y = 0
-        self.y_velocity = player_2_velocity
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-        self.health = 100
-        self.last_shot = pygame.time.get_ticks()
-
-    def update(self):
-        self.rect.y += self.y_velocity
-        cooldown = 500  # milliseconds
-        global bullet_speed_2
-        # movement inputs
-        key = pygame.key.get_pressed()
-        if key[pygame.K_LEFT] and self.rect.left > screen_width/2:
-            self.rect.x -= speed
-        if key[pygame.K_RIGHT] and self.rect.right < screen_width:
-            self.rect.x += speed
-
-        time_now = pygame.time.get_ticks()
-
-        if colour == 1 and key[pygame.K_UP] and time_now - self.last_shot > cooldown:
-            bullet = Bullets2(self.rect.centerx, self.rect.bottom)
-            bullet_group.add(bullet)
-            bullet_speed_2 = 5
-            self.last_shot = time_now
-
-        if colour == 2 and key[pygame.K_UP] and time_now - self.last_shot > cooldown:
-            bullet2 = Bullets2(self.rect.centerx, self.rect.top)
-            bullet_group.add(bullet2)
-            bullet_speed_2 = -5
+            bullet_speed = 5
             self.last_shot = time_now
 
 
 class Bullets(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Graphics/Bullet.png')
+        self.image = pygame.image.load('Graphics/Bullet.png').convert()
         self.rect = self.image.get_rect()
         self.rect.center = [x, y]
 
     def update(self):
-        self.rect.y += bullet_speed_1
+        self.rect.y += bullet_speed
         if self.rect.bottom > screen_height:
             self.kill()
         if self.rect.top < 0:
             self.kill()
 
 
-class Bullets2(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load('Graphics/Bullet.png')
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
-
-    def update(self):
-        self.rect.y += bullet_speed_2
-        if self.rect.bottom > screen_height:
-            self.kill()
-        if self.rect.top < 0:
-            self.kill()
-
-
-def colour_change():
+def gravity_change():
     # global variables
     global colour
-    global clicking
-    global floor_white_location_y
-    global floor_black_location_y
-    global bullet_speed_1
-    global bullet_speed_2
-
+    global bullet_speed
     key = pygame.key.get_pressed()
     if key[pygame.K_COMMA] and colour == 2:
         colour = 1
         player.y_velocity = 5
-        player2.y_velocity = -5
-        painting.clear()
-        bullet_speed_1 = -5
-        bullet_speed_2 = 5
-        floor_white_location_y = 0
-        floor_black_location_y = 905
+        bullet_speed = -5
 
     if key[pygame.K_PERIOD] and colour == 1:
         colour = 2
         player.y_velocity = -5
-        player2.y_velocity = 5
-        painting.clear()
-        bullet_speed_1 = 5
-        bullet_speed_2 = -5
-        floor_white_location_y = 905
-        floor_black_location_y = 0
-
-    location = pygame.mouse.get_pos()
-    mouse = pygame.mouse.get_pressed()
-    if mouse[0]:
-        pygame.draw.circle(screen, active_colour, location, 30)
-        painting.append((active_colour, location, 30))
-
-    draw_painting(painting)
+        bullet_speed = 5
 
 
 player_group = pygame.sprite.Group()
 
-player = Player1(int(screen_width / 4), screen_height - 500)
-player2 = Player2(int(screen_width / 1.3), screen_height - 500)
-player_group.add(player, player2)
+player = Player(int(screen_width / 4), screen_height - 500)
+player_group.add(player)
 
 bullet_group = pygame.sprite.Group()
 
@@ -214,12 +114,8 @@ while run:
     screen.blit(background_white, (0, 0))
     screen.blit(background_black, (screen_width / 2, 0))
 
-    # draws the ground instead of using an image to get them
-    floor_white = pygame.draw.rect(screen, (217, 217, 217), (floor_white_location_x, floor_white_location_y, 960, 175), 100)
-    floor_black = pygame.draw.rect(screen, (51, 51, 51), (floor_black_location_x, floor_black_location_y, 960, 175), 100)
-
     # allows the colour change function to run within the game
-    colour_change()
+    gravity_change()
 
     # allows the players to run within the game
     player_group.update()
@@ -239,10 +135,8 @@ while run:
             run = False
 
     # player collision with the floors, sets the players velocity to 0 so they stop falling
-    if player.rect.colliderect(floor_black):
-        player.y_velocity = 0
-    if player2.rect.colliderect(floor_white):
-        player2.y_velocity = 0
+    #if player.rect.colliderect(floor_black):
+    #    player.y_velocity = 0
 
     # draws the line separating the two players on the screen
     pygame.draw.line(screen, (150, 200, 220), ((screen_width/2) - 2.5, 0), ((screen_width/2) - 2.5, screen_height), 5)
