a21e7026fc6157076b7a24f339ca857a84fc6c0d
katiem1013 <katiem1013>
2023-04-10 19:12:44 +0100

    set levels to a different file, starting setting up levels

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 import pygame
 import time
+from Levels import level1, level2, screen, screen_width, screen_height, spike_group
+
 pygame.init()
+
+# what level is being selected
+start_menu = True
+level_1 = False
+level_2 = False
+
+# game variables
+game_paused = False
 
 # slows down the animations
 start_frame = time.time()
 noi = 6
 frames_per_second = 10
 
-# screen variables
-screen_width = 1920
-screen_height = 1080
 
-# screen setup
-screen = pygame.display.set_mode((screen_width, screen_height))
+
+
 pygame.display.set_caption('Assessment 3')
 clock = pygame.time.Clock()  # clock to set the frame rate
 
-# tile map
-tile_size = 64
 
 # screen flipping variables
 gravity = 1  # 1 has the player at the bottom, 2 has the player at the top
@@ -125,7 +130,7 @@ class Player(pygame.sprite.Sprite):
                 self.y_velocity = -5
 
         # checking for collision
-        for tile in world.tile_list:
+        for tile in level1.tile_list:
 
             # check for collision in x direction
             if tile[1].colliderect(self.rect.x + speed, self.rect.y, self.width, self.height):
@@ -214,163 +219,6 @@ player = Player(int(screen_width / 4), screen_height - 500)
 player_group.add(player)
 
 
-# spikes that will deal damage to the player when stood on
-class Spike(pygame.sprite.Sprite):
-    def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        spikes = pygame.image.load('Graphics/Spike.png')
-        self.image = pygame.transform.scale(spikes, (tile_size, tile_size))
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-
-
-# sets up the tile map to make the world
-class World:
-    def __init__(self, data):
-        self.tile_list = []
-
-        # tile map images
-        main_floor = pygame.image.load('Graphics/FloorMain.png')
-        right_side = pygame.image.load('Graphics/SideLeft.png')
-        left_side = pygame.image.load('Graphics/SideRight.png')
-        main_roof = pygame.image.load('Graphics/BottomMiddle.png')
-        inside_bottom_right = pygame.image.load('Graphics/InsideBottomRight.png')
-        inside_bottom_left = pygame.image.load('Graphics/InsideBottomLeft.png')
-        inside_top_right = pygame.image.load('Graphics/InsideTopRight.png')
-        inside_top_left = pygame.image.load('Graphics/InsideTopLeft.png')
-        outside_top_left = pygame.image.load('Graphics/CornerLeft.png')
-        outside_top_right = pygame.image.load('Graphics/CornerRight.png')
-        outside_bottom_left = pygame.image.load('Graphics/BottomLeft.png')
-        outside_bottom_right = pygame.image.load('Graphics/BottomRight.png')
-
-        # sets the numbers for each tile to be added to the list
-        row_count = 0
-        for row in data:
-            col_count = 0
-            for tile in row:
-                if tile == 1:
-                    image = main_floor
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 2:
-                    image = left_side
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 3:
-                    image = right_side
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 4:
-                    image = main_roof
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 5:
-                    image = inside_bottom_right
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 6:
-                    image = inside_bottom_left
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 7:
-                    image = inside_top_right
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 8:
-                    image = inside_top_left
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 9:
-                    image = outside_top_left
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 10:
-                    image = outside_top_right
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 11:
-                    image = outside_bottom_left
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-                if tile == 12:
-                    image = outside_bottom_right
-                    image_rect = image.get_rect()
-                    image_rect.x = col_count * tile_size
-                    image_rect.y = row_count * tile_size
-                    tile = (image, image_rect)
-                    self.tile_list.append(tile)
-
-                if tile == 13:
-                    spikes = Spike(col_count * tile_size, row_count * tile_size)
-                    spike_group.add(spikes)
-
-                col_count += 1
-            row_count += 1
-
-    def draw(self):
-        for tile in self.tile_list:
-            screen.blit(tile[0], tile[1])
-
-
-world_data = [
-    [8, 4, 4, 7, 8, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 7, 8, 4, 4, 7],
-    [2, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [2, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [2, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [2, 0, 0, 3, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [2, 0, 0, 11, 12, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 11, 12, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 13, 13, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 9, 10, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 2, 0, 0, 3],
-    [6, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 5, 6, 1, 1, 5]
-]
-
-spike_group = pygame.sprite.Group()
-
-world = World(world_data)
-
-
 class Bullets(pygame.sprite.Sprite):
     def __init__(self, x, y):
         pygame.sprite.Sprite.__init__(self)
@@ -428,25 +276,30 @@ cool_down_GC = 0
 run = True
 while run:
 
-    # makes the background appear on the screen
-    screen.blit(background, (0, 0))
+    if start_menu is False:
+        # makes the background appear on the screen
+        screen.blit(background, (0, 0))
+        # allows the gravity change function to run within the game
+        gravity_change()
 
-    # allows the gravity change function to run within the game
-    gravity_change()
+        # allows the players to run within the game
+        player_group.update()
+        player_group.draw(screen)
+        # allows the players bullets to run within the game
+        bullet_group.update()
+        bullet_group.draw(screen)
 
-    # draws the level onto the screen
-    world.draw()
+    if level_1 is True:
+        # draws the level onto the screen
+        level1.draw()
+        # draws the spikes onto the screen
+        spike_group.draw(screen)
 
-    # allows the players to run within the game
-    player_group.update()
-    player_group.draw(screen)
-
-    # allows the players bullets to run within the game
-    bullet_group.update()
-    bullet_group.draw(screen)
-
-    # draws the spikes onto the screen
-    spike_group.draw(screen)
+    if level_2 is True:
+        # draws the level onto the screen
+        level2.draw()
+        # draws the spikes onto the screen
+        spike_group.draw(screen)
 
     if player.current_health <= 0:
         run = False
@@ -460,6 +313,8 @@ while run:
         # escape turns run to false
         if key[pygame.K_ESCAPE]:
             run = False
+
+
 
     pygame.display.flip()
     pygame.display.update()
